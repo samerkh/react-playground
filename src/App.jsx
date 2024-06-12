2127b5f34f6a576b7eb592d0a6e02eed320ffe30
Samer Alkhatib <khatib@cotede.ps>
2024-06-12 08:06:11 +0300

    Refactor components into separate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
+import CoreConcepts from "./components/CoreConcepts";
+import Header from "./components/Header";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-export function Header() {
-  const index = genRandomInt(2);
-  const description = reactDescriptions[index];
-
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-export function CoreConcepts({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
