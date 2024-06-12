df412c18e92abaeb0cbe6e4f5c421adb7efd9a7a
Samer Alkhatib <khatib@cotede.ps>
2024-06-12 07:28:01 +0300

    Create dynamic description

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-export function Header() {
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const index = genRandomInt(2);
+  const description = reactDescriptions[index];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
